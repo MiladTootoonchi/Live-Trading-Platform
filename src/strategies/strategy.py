ebf9cefed56503ac116d7bea345101084e305389
MiladTootoonchi <milad.tootoonchi04@gmail.com>
2025-07-02 14:22:21 +0200

    added the strategies in the strategy dictionary

--- src/strategies/strategy.py
+++ src/strategies/strategy.py
@@ -6,7 +6,12 @@ given position information. Then it will return a signal with quantity of order.
 from enum import Enum
 from typing import Callable, Dict, Any
 
-from . import bollinger_bands_strategy, macd, mean_reversion, momentum, moving_average_strategy, rsi
+from .bollinger_bands_strategy import bollinger_bands_strategy
+from .macd import macd_strategy
+from .mean_reversion import mean_reversion_strategy
+from .momentum import momentum_strategy
+from .moving_average_strategy import moving_average_strategy
+from .rsi import rsi_strategy
 
 class SideSignal(Enum):
     """Enum representing possible order sides."""
@@ -60,6 +65,12 @@ def rule_based_strategy(position_data: dict) -> tuple[SideSignal, int]:
 
 strategies = {
     "rule_based_strategy": rule_based_strategy,
+    "bollinger_bands_strategy": bollinger_bands_strategy,
+    "macd_strategy": macd_strategy,
+    "mean_reversion_strategy": mean_reversion_strategy,
+    "momentum_strategy": momentum_strategy,
+    "moving_average_strategy": moving_average_strategy,
+    "rsi_strategy": rsi_strategy,
 }
 
 
