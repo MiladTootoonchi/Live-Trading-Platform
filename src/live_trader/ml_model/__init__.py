3c79ee1f55143c6623f90c40f8958c240214a785
MiladTootoonchi <milad.tootoonchi04@gmail.com>
2026-02-05 15:00:55 +0100

    changed names to fit file content

--- src/live_trader/ml_model/__init__.py
+++ src/live_trader/ml_model/__init__.py
@@ -1,11 +1,11 @@
-from .training import ML_Pipeline
+from .ml_pipeline import MLStrategyBase
 from .evaluations import brier, brier_score
 from .layers import Patchify, GraphMessagePassing, ExpandDims, AutoencoderClassifierLite
-from .ml_strategies import basic_lstm, attention_bilstm, tcn_lite, patchtst_lite, gnn_lite, nad_lite, cnn_gru_lite
+from .ml_strategies import LSTM, BiLSTM, TCN, PatchTST, GNN, NAD, CNNGRU
 from .utils import ProbabilisticClassifier
 from .data import MLDataPipeline
 
-__all__ = ["ML_Pipeline", "brier", "brier_score", "ProbabilisticClassifier", "MLDataPipeline",
+__all__ = ["MLStrategyBase", "brier", "brier_score", "ProbabilisticClassifier", "MLDataPipeline",
            "Patchify", "GraphMessagePassing", "ExpandDims", "AutoencoderClassifierLite",
-           "basic_lstm", "attention_bilstm", "tcn_lite", "patchtst_lite", "gnn_lite", "nad_lite", "cnn_gru_lite"]
+           "LSTM", "BiLSTM", "TCN", "PatchTST", "GNN", "NAD", "CNNGRU"]
 __author__ = "Milad Tootoonchi"
