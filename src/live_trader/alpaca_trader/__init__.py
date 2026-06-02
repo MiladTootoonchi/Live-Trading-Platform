6ae2849e191c0bdec789847c844b20664ee4c773
Milad Tootoonchi <milad.tootoonchi04@gmail.com>
2026-06-02 15:54:44 +0200

    Delete src/live_trader directory copy

--- src/live_trader/alpaca_trader/__init__.py
+++
@@ -1,5 +0,0 @@
-from .live_trading import AlpacaTrader
-from .order import SideSignal
-
-__all__ = ["AlpacaTrader", "SideSignal"]
-__author__ = "Milad Tootoonchi"
--- src/live_trader/tree_based_models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tree_strategies import RandomForest, LGBM, XGB, CatBoost
-
-__all__ = ["RandomForest", "LGBM", "XGB", "CatBoost"]
-__author__ = "Milad Tootoonchi"
